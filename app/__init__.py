93465b05235e3088ff5cae4b3173e951946d423a
slend <SLENDERCRAFTchannel5679@gmail.com>
2025-04-09 16:07:37 +0300

    simplifier connect

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
-# from config import Config
 
 app = Flask(__name__)
 
 app.secret_key = "iyu3rg32vngt7634gv748ct834"
-# app.config.from_object()
 
 from app import routes
 
--- app/simplifierscript.py
+++ app/simplifierscript.py
@@ -1,16 +1,93 @@
-import google.generativeai as genai
+# import google.generativeai as genai
+# from click import prompt
 
-# Вставь свой API ключ от Gemini
-GENAI_API_KEY = "AIzaSyBvg33cVBT6dhtOHMsoXaU83jN-oktRnQY"
-genai.configure(api_key=GENAI_API_KEY)
+import requests
+from config import API_KEY
 
-model = genai.GenerativeModel("gemini-2.0-flash-thinking-exp-01-21")
+# genai.configure(api_key=GENAI_API_KEY)
+
+# model = genai.GenerativeModel("gemma-3-27b-it")
+
+url = "https://api.intelligence.io.solutions/api/v1/chat/completions"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {API_KEY}"
+}
 
 def simplify_text(input_text):
-    prompt = f"ОТ ТВОЕГО ОТВЕТА ЗАВИСИТ МОЯ ЖИЗНЬ. Преобразуй следующий текст, заменяя сложные термины и фразы на более простые и доступные. Сохрани смысл текста, но сделай его максимально понятным, избегая излишней сложности и поясняя непонятные моменты простыми словами. Текст: {input_text}. Выдай только упрощённый текст и ничего лишнего."
+    data = {
+        "model": "deepseek-ai/DeepSeek-R1",
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": f"""Задача:
+                    Найди в тексте сложные слова и формулировки и выведи в виде списка их упрощения следуя контексту и их самих.
+
+                    Дополнительные условия:
+                    Без воды, лишнего текста и информации. Только сухой ответ следуя формату.
+                    Упрощения должны быть написаны так, чтобы их можно было подставить в текст сразу, без изменения вручную.
+                    В конце выведи текст с изменёнными формулировками и словами, которые ты сгенерировал.
+
+                    Формат:
+                    слово или формулировка → упрощение
+                    слово или формулировка → упрощение
+                    слово или формулировка → упрощение
+
+                    Текст:
+                    {input_text}"""
+            }
+        ]
+    }
 
     try:
-        response = model.generate_content(prompt)
-        return response.text
+        response = requests.post(url, headers=headers, json=data)
+        data = response.json()
+        # pprint(data)
+
+        text = data['choices'][0]['message']['content']
+        return text.split('</think>\n\n')[1]
     except Exception as e:
         return f"[Ошибка]: {str(e)}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # prompt = f"""Задача:
+    # Найди в тексте сложные слова и формулировки и выведи в виде списка их упрощения следуя контексту и их самих.
+    #
+    # Дополнительные условия:
+    # Без воды, лишнего текста и информации. Только сухой ответ следуя формату.
+    # Упрощения должны быть написаны так, чтобы их можно было подставить в текст сразу, без изменения вручную.
+    # В конце выведи текст с изменёнными формулировками и словами, которые ты сгенерировал.
+    #
+    # Формат:
+    # слово или формулировка → упрощение
+    # слово или формулировка → упрощение
+    # слово или формулировка → упрощение
+    #
+    # Текст:
+    # {input_text}"""
+
+    # prompt = f"ОТ ТВОЕГО ОТВЕТА ЗАВИСИТ МОЯ ЖИЗНЬ. Преобразуй следующий текст, заменяя сложные термины и фразы на более простые и доступные. Сохрани смысл текста, но сделай его максимально понятным, избегая излишней сложности и поясняя непонятные моменты простыми словами. Текст: {input_text}. Выдай только упрощённый текст и ничего лишнего."
+
+    # try:
+    #     response = model.generate_content(prompt)
+    #     return response.text
+    # except Exception as e:
+    #     return f"[Ошибка]: {str(e)}"
